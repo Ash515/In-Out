c2218c714200ec9f4d6cd8efcab9fae0c268787f
Ash515 <aaashwin515@gmail.com>
2022-11-23 21:38:13 +0530

    Added files

---
+++ README.md
@@ -0,0 +1,6 @@
+# In-Out
+Microservice for track the employees presence and absence status and also manage the salary.
+
+
+Developed By
+- Ashwin Kumar R 
---
+++ public/app.js
@@ -0,0 +1,19 @@
+function checkin(){
+   
+    var In = document.getElementById("checkin");
+    if(In){
+        document.getElementById("status").innerHTML= "Checked In Successfully !";
+    }
+}
+
+function checkout(){
+    
+    var In = document.getElementById("checkout");
+    if(In){
+        document.getElementById("status").innerHTML= "Checked out Successfully !";
+    }
+    
+    
+    
+}
+
